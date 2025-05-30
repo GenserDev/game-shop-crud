f3103a307974f2c8d32ad2b8ca08cac2eb59ab25
GenserDev <genserdev@gmail.com>
2025-05-30 12:03:10 -0600

    build: frontend

--- Laboratory/src/app.js
+++ Laboratory/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 
 // Cargar rutas
 const productRoutes = require('./routes/products');
-app.use('/api/productos', productRoutes);
+app.use('/api/products', productRoutes); 
 
 module.exports = app;
